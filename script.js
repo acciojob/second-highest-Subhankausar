f320be455d092e316595ea5e3fba34dd971b6beb
Subhan Kausar <126421273+Subhankausar@users.noreply.github.com>
2023-10-04 16:04:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your JS code here. If required.
+function secondHighest(arr) {
+    if (arr.length < 2) {
+        return -Infinity; // Not enough elements to find a second-highest
+    }
+
+    let max = -Infinity;
+    let secondMax = -Infinity;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max) {
+            secondMax = max;
+            max = arr[i];
+        } else if (arr[i] > secondMax && arr[i] !== max) {
+            secondMax = arr[i];
+        }
+    }
+
+    if (secondMax === -Infinity) {
+        return -Infinity; // All elements are the same
+    }
+
+    return secondMax;
+}
